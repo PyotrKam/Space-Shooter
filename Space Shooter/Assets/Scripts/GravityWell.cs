e65416ca32c36941802659d5b9d11bbcf4770d5e
Pyotr <pyotrk@inbox.lv>
2024-12-05 21:04:41 +0200

    Add Planet
    Add in scene planet sprite

---
+++ Space Shooter/Assets/Scripts/GravityWell.cs
@@ -0,0 +1,39 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace SpaceShooter 
+{
+
+    [RequireComponent(typeof(CircleCollider2D))]
+    public class GravityWell : MonoBehaviour
+    {
+        [SerializeField] private float m_Force;
+        [SerializeField] private float m_Radius;
+
+        private void OnTriggerStay2D(Collider2D collision)
+        {
+            if (collision.attachedRigidbody == null) return;
+
+            Vector2 dir = transform.position - collision.transform.position;
+
+            float dist = dir.magnitude;
+
+            if (dist < m_Radius)
+            {
+                Vector2 force = dir.normalized * m_Force * (dist / m_Radius);
+                collision.attachedRigidbody.AddForce(force, ForceMode2D.Force);
+            }
+
+        }
+
+
+
+    }
+
+
+
+}
+
+
+
