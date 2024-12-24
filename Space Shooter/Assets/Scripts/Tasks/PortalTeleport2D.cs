37b73f78b6ba4b9995d2353b22a4861f179f8ecd
Pyotr <pyotrk@inbox.lv>
2024-12-24 14:51:07 +0200

    Trying with Teleport
    Add Try and Tasks scene

--- Space Shooter/Assets/Scripts/Tasks/PortalTeleport2D.cs
+++ Space Shooter/Assets/Scripts/Tasks/PortalTeleport2D.cs
@@ -5,17 +5,16 @@ using UnityEngine;
 public class PortalTeleport2D : MonoBehaviour
 {
     [SerializeField] private Transform destinationPortal;
-    [SerializeField] private Transform m_player;
-    [SerializeField] private Transform m_camera;
+    
 
 
 
-    private void OnTriggerEnter2D(Collider2D collision)
+    private void OnTriggerEnter2D(Collider2D other)
     {
-        
 
-        m_player.position = destinationPortal.position;
 
-        m_camera.position = new Vector3(destinationPortal.position.x, destinationPortal.position.y, m_camera.position.z);
+        other.transform.position = destinationPortal.position;
+
+       
     }
 }
