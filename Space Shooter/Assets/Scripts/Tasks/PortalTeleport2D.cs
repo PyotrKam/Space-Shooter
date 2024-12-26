4151f1e15efb272cfc5ed98d13a904dccde95c6d
Pyotr <pyotrk@inbox.lv>
2024-12-26 15:59:38 +0200

    Last version_17
    Add Teleport

--- Space Shooter/Assets/Scripts/Tasks/PortalTeleport2D.cs
+++ Space Shooter/Assets/Scripts/Tasks/PortalTeleport2D.cs
@@ -4,17 +4,22 @@ using UnityEngine;
 
 public class PortalTeleport2D : MonoBehaviour
 {
-    [SerializeField] private Transform destinationPortal;
-    
-
-
+    [SerializeField] private Transform destinationPortal; 
 
     private void OnTriggerEnter2D(Collider2D other)
     {
+        // Get the Rigidbody2D component from the object that entered the trigger
+        Rigidbody2D rb = other.GetComponent<Rigidbody2D>();
 
-
-        other.transform.position = destinationPortal.position;
-
-       
+        // If Rigidbody2D exists, move it
+        if (rb != null)
+        {
+            rb.position = destinationPortal.position;
+        }
+        // If there is no Rigidbody2D, move the object using transform
+        else
+        {
+            other.transform.position = destinationPortal.position;
+        }
     }
 }
