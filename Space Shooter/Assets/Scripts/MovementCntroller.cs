d4e17e01451ed3e5661ae9ad0b9da4149f66aa13
Pyotr <pyotrk@inbox.lv>
2025-01-03 12:09:31 +0200

    Version_Spaceship_shooting
    Added shots and their limitations

--- Space Shooter/Assets/Scripts/MovementCntroller.cs
+++ Space Shooter/Assets/Scripts/MovementCntroller.cs
@@ -22,16 +22,28 @@ namespace SpaceShooter
 
         [SerializeField] private ControlMode m_ControlMode;
 
+        [SerializeField] private PointerClickHold m_MobileFirePrimary;
+        [SerializeField] private PointerClickHold m_MobileFireSecondary;
+
+            
 
         private void Start()
         {
             if (m_ControlMode == ControlMode.Keyboard)
             {
                 m_MobileJoystick.gameObject.SetActive(false);
+
+                m_MobileFirePrimary.gameObject.SetActive(false);
+                m_MobileFireSecondary.gameObject.SetActive(false);
+
+
             }
             else
             {
                 m_MobileJoystick.gameObject.SetActive(true);
+
+                m_MobileFirePrimary.gameObject.SetActive(true);
+                m_MobileFireSecondary.gameObject.SetActive(true);
             }
 
         }
@@ -53,6 +65,17 @@ namespace SpaceShooter
             var dot = Vector2.Dot(dir, m_TargetShip.transform.up);
             var dot2 = Vector2.Dot(dir, m_TargetShip.transform.right);
 
+            if (m_MobileFirePrimary.IsHold == true)
+            {
+                m_TargetShip.Fire(TurretMode.Primary);
+            }
+
+            if (m_MobileFireSecondary.IsHold == true)
+            {
+                m_TargetShip.Fire(TurretMode.Secondary);
+            }
+
+
             m_TargetShip.ThrustControl = Mathf.Max(0, dot);
             m_TargetShip.TorqueControl = -dot2;
 
@@ -70,6 +93,16 @@ namespace SpaceShooter
             if (Input.GetKey(KeyCode.LeftArrow)) torque = 1.0f;
 
             if (Input.GetKey(KeyCode.RightArrow)) torque = -1.0f;
+
+            if (Input.GetKey(KeyCode.Space)) 
+            {
+                m_TargetShip.Fire(TurretMode.Primary);
+            }
+
+            if (Input.GetKey(KeyCode.X))
+            {
+                m_TargetShip.Fire(TurretMode.Secondary);
+            }
 
             m_TargetShip.ThrustControl = thrust;
             m_TargetShip.TorqueControl = torque;
--- Space Shooter/Assets/Scripts/SpaceShip.cs
+++ Space Shooter/Assets/Scripts/SpaceShip.cs
@@ -62,11 +62,15 @@ namespace SpaceShooter
 
             m_Rigid.inertia = 1; //Set the inerial force, to make it easier to balance the balance of power
 
+            InitOffensive();
+
         }       
 
         private void FixedUpdate() //Update not in frames, but in a certain amount of time
         {
             UpdateRigidBody();
+
+            UpdateEnergyRegen();
         }
 
         #endregion
@@ -86,6 +90,77 @@ namespace SpaceShooter
 
         }
 
+
+        [SerializeField] private Turret[] m_Turrets;
+        public void Fire(TurretMode mode)
+        {
+            for (int i = 0; i < m_Turrets.Length; i++)
+            {
+                if (m_Turrets[i].Mode == mode) 
+                {
+                    m_Turrets[i].Fire();
+                }       
+            }
+        }
+
+        [SerializeField] private int m_MaxEnergy;
+        [SerializeField] private int m_MaxAmmo;
+        [SerializeField] private int m_EnergyRegenPerSecond;
+
+        private float m_PrimaryEnergy;
+        private int m_SecondaryAmmo;
+
+         public void AddEnergy(int e)
+        {
+            m_PrimaryEnergy = Mathf.Clamp(m_PrimaryEnergy + e, 0, m_MaxEnergy);
+        }
+
+        public void AddAmmo(int ammo)
+        {
+            m_SecondaryAmmo = Mathf.Clamp(m_SecondaryAmmo + ammo, 0, m_MaxAmmo);
+        }
+
+        private void InitOffensive()
+        {
+            m_PrimaryEnergy = m_MaxEnergy;
+            m_SecondaryAmmo = m_MaxAmmo;
+        }
+
+        private void UpdateEnergyRegen()
+        {
+            m_PrimaryEnergy += (float) m_EnergyRegenPerSecond * Time.fixedDeltaTime;
+            m_PrimaryEnergy = Mathf.Clamp(m_PrimaryEnergy, 0, m_MaxEnergy);
+        }
+
+        public bool DrawAmmo(int count)
+        {
+            if (count == 0)
+            {
+                return true;
+            }
+
+            if (m_SecondaryAmmo >= count)
+            {
+                m_SecondaryAmmo -= count;
+                return true;
+            }
+            return false;
+        }
+
+        public bool DrawEnergy(int count)
+        {
+            if (count == 0)
+            {
+                return true;
+            }
+
+            if (m_PrimaryEnergy >= count)
+            {
+                m_PrimaryEnergy -= count;
+                return true;
+            }
+            return false;
+        }
 
     }
 
