4e3ef1559b13ecddc9fa3c6de9cf144197ae4178
Pyotr <pyotrk@inbox.lv>
2025-01-21 21:27:52 +0200

    Main menu

---
+++ Space Shooter/Assets/Scripts/LevelLogic/CompletionsCondition/LevelCompletionScore.cs
@@ -0,0 +1,33 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace SpaceShooter
+{
+    public class LevelCompletionScore : LevelCondition
+    {
+        [SerializeField] private int m_Score;
+
+        public override bool IsCompleted
+        {
+            
+
+            get
+            {
+                if (Player.Instance.ActiveShip == null) return false;
+
+                if (Player.Instance.Score >= m_Score)
+                {
+                    return true;
+                }
+
+                return false;
+               
+            }
+        }
+
+
+    }
+}
+
+   
---
+++ Space Shooter/Assets/Scripts/LevelLogic/CompletionsCondition/LevelCondition.cs
@@ -0,0 +1,15 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace SpaceShooter
+{
+    public abstract class LevelCondition : MonoBehaviour
+    {
+        public virtual bool IsCompleted { get; }
+    }
+
+}
+
+
+   
